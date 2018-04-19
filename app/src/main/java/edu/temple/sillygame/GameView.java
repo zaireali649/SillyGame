0bb92d56e6a5ede6a645b14d5d072e9835a06962
Zaire Ali <zaireali649@gmail.com>
2018-04-19 03:06:57 -0400

    - implement collision listener - fix Y bounds

--- app/src/main/java/edu/temple/sillygame/GameView.java
+++ app/src/main/java/edu/temple/sillygame/GameView.java
@@ -4,21 +4,28 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
+
+
 
 public class GameView extends View {
 
-    int canvasWidth, canvasHeight;
-    Paint myColor;
+    int canvasWidth, canvasHeight, buttonX = 100, buttonY = 100;
+    Paint myColor, scoreColor;
 
     public GameObject enemy, otherEnemy;
 
-    int myX, myY, mySize;
+    public int myX, myY, mySize;
+
+    int score = 0;
 
     Context context;
 
@@ -28,6 +35,14 @@ public class GameView extends View {
         super(context, attrs);
 
         this.context = context;
+
+        myX = 0;
+        myY = 0;
+        mySize = 100;
+
+        scoreColor = new Paint();
+        scoreColor.setColor(Color.BLACK);
+
         myColor = new Paint();
         myColor.setColor(Color.BLUE);
         enemy = new GameObject(0,0,80, Color.RED, 20, null) {
@@ -85,7 +100,8 @@ public class GameView extends View {
                 if (yDirection == 0) {
                     if ((y - movePixels) > 0) y -= movePixels;
                     else yDirection = 1;
-                } else {
+                }
+                else {
                     if ((y + movePixels) + size < canvasHeight) y += movePixels;
                     else yDirection = 0;
                 }
@@ -96,12 +112,15 @@ public class GameView extends View {
 
         };
 
+        otherEnemy.addCollisionListener(new GameObject.CollisionListener(){
+            @Override
+            public void collisionOccurred(){
+                score = score + 1;
+            }
+        });
+
         otherEnemy.start();
 
-
-        myX = 0;
-        myY = 0;
-        mySize = 100;
 
     }
 
@@ -116,14 +135,40 @@ public class GameView extends View {
 
         GameObject object;
 
-        while ((object = getObjectToBeDrawn()) != null)
+        while ((object = getObjectToBeDrawn()) != null) {
+            updatePlayer(object);
             drawObject(canvas, object);
+        }
 
         values = ((Rotatable) context).getRotationInfo();
 
-
         drawSelf(canvas, values);
 
+        drawScore(canvas, score);
+    }
+
+    private void updatePlayer(GameObject object) {
+        object.setpX(myX);
+        object.setpY(myY);
+        object.setpSize(mySize);
+    }
+
+    private void drawScore(Canvas canvas, int score) {
+
+        final float testTextSize = 32f;
+
+        // Get the bounds of the text, using our testTextSize.
+        scoreColor.setTextSize(testTextSize);
+        Rect bounds = new Rect();
+        scoreColor.getTextBounds(String.valueOf(score), 0, String.valueOf(score).length(), bounds);
+
+        // Calculate the desired size as a proportion of our testTextSize.
+        float desiredTextSize = testTextSize * 200 / bounds.width();
+
+        // Set the paint for that size.
+        scoreColor.setTextSize(desiredTextSize);
+
+        canvas.drawText(String.valueOf(score), 10, canvas.getHeight() - 10, scoreColor);
     }
 
     private synchronized void addToBeDrawn(GameObject gameObject) {
@@ -139,23 +184,36 @@ public class GameView extends View {
         int multiplier = 50;
 
         if (values[1] < 0) {
-            if (myY + mySize - (int) (Math.abs(values[1]) * multiplier) > 0)
+            if (myY + (int) (Math.abs(values[1]) * multiplier) > 0)
                 myY = myY + (int) (Math.abs(values[1]) * multiplier);
-        } else {
-            if (myY - (int) (Math.abs(values[1]) * multiplier) + mySize < canvasHeight)
+        }
+        else {
+            if (myY - (int) (Math.abs(values[1]) * multiplier) < canvasHeight)
             myY = myY - (int) (Math.abs(values[1]) * multiplier);
         }
 
         if (values[2] < 0) {
-            if (myX - (int) (Math.abs(values[2]) * multiplier) - mySize > 0)
+            if (myX - (int) (Math.abs(values[2]) * multiplier) > 0)
             myX = myX - (int) (Math.abs(values[2]) * multiplier);
         } else {
-            if (myX + (int) (Math.abs(values[2]) * multiplier) + mySize < canvasWidth)
+            if (myX + (int) (Math.abs(values[2]) * multiplier) < canvasWidth)
             myX = myX + (int) (Math.abs(values[2]) * multiplier);
         }
 
+        if (myY < 0){
+            myY = 0;
+        }
+        else if (myY > canvasHeight)
+        {
+            myY = canvasHeight;
+        }
+
+
+
         canvas.drawCircle(myX, myY, mySize, myColor);
     }
+
+
 
     private void drawObject(Canvas canvas, GameObject object) {
         canvas.drawRect(object.x
@@ -166,19 +224,34 @@ public class GameView extends View {
     }
 
 
-    abstract class GameObject {
+    abstract static class GameObject {
         int x, y, size;
+        int pX, pY, pSize;
         Paint color;
         private ObjectThread thread;
+        private List<CollisionListener> listeners = new ArrayList<CollisionListener>();
 
         GameObject(int x, int y, int size, int color, int delay, Object data) {
             this.x = x;
             this.y = y;
             this.size = size;
+
+            this.pX = 0;
+            this.pY = 0;
+            this.pSize = 10;
+
             Paint p = new Paint();
             p.setColor(color);
             this.color = p;
             this.thread = new ObjectThread(this, delay, data);
+
+            Thread thread = new Thread(){
+                public void run(){
+                    collisionChecker();
+                }
+            };
+
+            thread.start();
         }
 
         public int getX() {
@@ -211,6 +284,75 @@ public class GameView extends View {
 
         public abstract void performAction (Object data);
 
+
+        public void addCollisionListener(CollisionListener listener) {
+            listeners.add(listener);
+        }
+
+        public void collisionChecker() {
+            while(true) {
+                // Notify everybody that may be interested.
+                for (CollisionListener cl : listeners) {
+                    //Circle c = new Circle(pX, pY, pSize);
+                    //Circle c = new Circle(0f, 0f, 0f);
+
+                    //Rectangle r = new Rectangle(x, y, size, size);
+                    if (collides()){
+                        cl.collisionOccurred();
+                    }
+                }
+            }
+        }
+
+        private boolean collides() {
+            float closestX = clamp((float) pX, x, x + size );
+            float closestY = clamp((float) pY, y - size, y);
+
+            float distanceX = (float) pX - closestX;
+            float distanceY = (float) (pY - closestY);
+
+            return Math.pow(distanceX, 2) + Math.pow(distanceY, 2) < Math.pow(pSize, 2);
+        }
+
+        public static float clamp(float value, double min, double max) {
+            double x = value;
+            if (x < min) {
+                x = min;
+            } else if (x > max) {
+                x = max;
+            }
+            return (float) x;
+        }
+
+        public int getpX() {
+            return pX;
+        }
+
+        public void setpX(int pX) {
+            this.pX = pX;
+        }
+
+        public int getpY() {
+            return pY;
+        }
+
+        public void setpY(int pY) {
+            this.pY = pY;
+        }
+
+        public int getpSize() {
+            return pSize;
+        }
+
+        public void setpSize(int pSize) {
+            this.pSize = pSize;
+        }
+
+
+        public interface CollisionListener {
+            void collisionOccurred();
+        }
+
         class ObjectThread extends Thread {
 
             int delay;
@@ -222,7 +364,6 @@ public class GameView extends View {
                 this.object = object;
                 this.delay = delay;
                 this.data = data;
-
             }
 
             public void run () {
